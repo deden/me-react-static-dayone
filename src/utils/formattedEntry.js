9d8fb6c830a60b3c6d893dee6da15fbb507eeaee
Deden Ramadhan <deden96@gmail.com>
2017-11-29 22:14:09 +0700

    fix empty title

--- src/utils/formattedEntry.js
+++ src/utils/formattedEntry.js
@@ -2,7 +2,7 @@
 export default entry => {
   const title = entry.text.slice(0, entry.text.indexOf('\n')).trim()
   const body = entry.text.slice(entry.text.indexOf('\n')).trim()
-  const url = title.replace(/[^a-z0-9]/gi, '_').toLowerCase()
+  const url = title.replace(/[^a-z0-9]/gi, '_').toLowerCase() || entry.uuid
   return {
     ...entry,
     title,
--- tests/utils/formattedEntry.spec.js
+++ tests/utils/formattedEntry.spec.js
@@ -94,4 +94,40 @@ describe('formattedEntry', () => {
     const actual = formattedEntry(entry)
     expect(actual).toEqual(expected)
   })
+
+  it('should accept any first line of entry as title', () => {
+    const entry = {
+      "text" : "This app really slick, superb design style and very  well ‘handcrafted’ in every pixel.\n\nI wonder if I can import my Calendar entries into this app.\nIn meanwhile I will find out, just a sec.",
+      "uuid" : "6D558974A12B40D98552EF31BB00C166",
+    }
+
+    const expected = {
+      "text" : "This app really slick, superb design style and very  well ‘handcrafted’ in every pixel.\n\nI wonder if I can import my Calendar entries into this app.\nIn meanwhile I will find out, just a sec.",
+      "uuid" : "6D558974A12B40D98552EF31BB00C166",
+      "title" : "This app really slick, superb design style and very  well ‘handcrafted’ in every pixel.",
+      "body" : "I wonder if I can import my Calendar entries into this app.\nIn meanwhile I will find out, just a sec.",
+      "url" : "this_app_really_slick__superb_design_style_and_very__well__handcrafted__in_every_pixel_"
+    }
+
+    const actual = formattedEntry(entry)
+    expect(actual).toEqual(expected)
+  })
+
+  it('should accept an entry with empty fist new line as empty title, with url copied from uuid instead', () => {
+    const entry = {
+      "text" : "\nThis app really slick, superb design style and very  well ‘handcrafted’ in every pixel.\n\nI wonder if I can import my Calendar entries into this app.\nIn meanwhile I will find out, just a sec.",
+      "uuid" : "6D558974A12B40D98552EF31BB00C166",
+    }
+
+    const expected = {
+      "text" : "\nThis app really slick, superb design style and very  well ‘handcrafted’ in every pixel.\n\nI wonder if I can import my Calendar entries into this app.\nIn meanwhile I will find out, just a sec.",
+      "uuid" : "6D558974A12B40D98552EF31BB00C166",
+      "title" : "",
+      "body" : "This app really slick, superb design style and very  well ‘handcrafted’ in every pixel.\n\nI wonder if I can import my Calendar entries into this app.\nIn meanwhile I will find out, just a sec.",
+      "url" : "6D558974A12B40D98552EF31BB00C166"
+    }
+
+    const actual = formattedEntry(entry)
+    expect(actual).toEqual(expected)
+  })
 })
